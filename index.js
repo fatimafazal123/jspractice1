19edb70a072a10a75d8633003cc8d528270ead17
Fatima Fazal <159261117+fatimafazal123@users.noreply.github.com>
2024-06-06 17:48:40 +0500

    js practice 1

---
+++ index.js
@@ -0,0 +1,12 @@
+//var weather=prompt("Check Your Area Weather Here:cold,rainy,sunny,snow.")
+var weather=prompt("City:karachi")
+if("karachi"){
+    document.write(
+     "<div class='card one mx-auto' style='width: 15rem;'>" +
+    "<img src='./img1.png' class='card-img-top w-100 ' alt='...'>" +
+    "<div class='card-body'>" +
+      "<h5 class='card-title three'>" + "Welcome" + "</h5>" +
+      "<p class='card-text three'>" + "Welcome to city of Lights." + "</p>" +
+    "</div>" +
+  "</div>"
+  );}
